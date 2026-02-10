6f83111f11afcc7edd616022e82534c3ea84806c
ltpatil <ltpatil.3088@gmail.com>
2026-02-10 11:00:05 +0530

    Basic express server setup with TypeScript

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+import serverConfig from './config/server.config';
+const app = express();
+app.listen(serverConfig.PORT,()=>{
+    console.log(`Server started at ${serverConfig.PORT}`);
+})

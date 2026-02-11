2dfb6fa8fb59d6b08c08bd857e02b8f4fcf87aba
ltpatil <ltpatil.3088@gmail.com>
2026-02-11 14:06:20 +0530

    setup for ioRedis and bullMQ with sample object and queue added

--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 export default{
-    PORT : process.env.PORT 
+    PORT : process.env.PORT,
+    REDIS_PORT : parseInt(process.env.REDIS_PORT || '6379'),
+    REDIS_HOST : process.env.REDIS_URL
 };
---
+++ src/jobs/sampleJob.ts
@@ -0,0 +1,22 @@
+import { Job } from "bullmq";
+
+import { IJob } from "../types/bullmqjbdefinition";
+
+export default class SampleJob implements IJob{
+    name : string;
+    payload?: Record<string, unknown>;
+    constructor(payload : Record<string,unknown>){
+        this.payload = payload;
+        this.name = this.constructor.name;
+    }
+    handle = () =>{
+        console.log("Handler of the job called");
+    };
+
+    failed = (job?: Job) => {
+        console.log("Job Failed");
+        if(job){
+            console.log(job.id);
+        }
+    };
+}
